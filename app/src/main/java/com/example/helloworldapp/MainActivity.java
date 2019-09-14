6c89705eca4595d631af69ff6251b291b49dd6f0
mandar <mandar.yawalkar@gmail.com>
2019-09-14 15:56:20 -0400

    Layout + create and join function

--- app/src/main/java/com/example/helloworldapp/MainActivity.java
+++ app/src/main/java/com/example/helloworldapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.helloworldapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,14 +12,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_page);
 
 
 
     }
 
     public void createPageOnClick(View V)
-        {
+    {
+//        Intent intent = new Intent(this,);
+//        startactivity();
+    }
+    public void joinPageOnClick(View V)
+    {
 
     }
 
