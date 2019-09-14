814d9304bc4aa91c3784a426d6bc41cc73781270
asacchetti11 <asacche@purdue.edu>
2019-09-14 18:20:57 -0400

    added date layout for create page

--- app/src/main/java/com/example/helloworldapp/CreateGroupPage.java
+++ app/src/main/java/com/example/helloworldapp/CreateGroupPage.java
@@ -2,9 +2,17 @@ package com.example.helloworldapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateGroupPage extends AppCompatActivity {
+
+
+    private DatePickerDialog.OnDateSetListener studyDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +21,21 @@ public class CreateGroupPage extends AppCompatActivity {
 
         //TextView Name = ((TextView)findViewById(R.id.InformationPageName));
 
+
+    }
+
+    public void dateOnClick(View v){
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dialog = new DatePickerDialog(
+                this,
+                android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                studyDateSetListener,
+                year,month,day);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
     }
 }
