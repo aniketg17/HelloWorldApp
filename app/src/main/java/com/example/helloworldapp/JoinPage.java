d7f699ae7ad7673765f7f952f19caca2a8d15d33
shayne243 <marques0@purdue.edu>
2019-09-14 21:53:30 -0400

    Merge remote-tracking branch 'remotes/origin/alessandro' into Shayne1
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/helloworldapp/JoinPage.java
+++ app/src/main/java/com/example/helloworldapp/JoinPage.java
@@ -1,14 +1,39 @@
 package com.example.helloworldapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class JoinPage extends AppCompatActivity {
 
+    String[] data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join_page);
+
+        data =  new String[]{"COURSE 1", "COURSE 2", "COURSE 3","COURSE 4"};
+
+        ListView courselistListView = (ListView) findViewById(R.id.courselistListView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+
+        courselistListView.setAdapter(adapter);
+
+        courselistListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent();
+                intent.putExtra("Key",data[position]);
+                intent.setClass(JoinPage.this,test.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/helloworldapp/MainActivity.java
+++ app/src/main/java/com/example/helloworldapp/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.helloworldapp;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView listView;
+    SearchView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
     }
 }
